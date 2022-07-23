7fdaa8076baf5a7d635a518b8cc4d46f639ed1c7
Jason Louv <jason@magick.me>
2022-07-22 19:40:43 -0500

    Initial commit

---
+++ Cookcademy/Cookcademy/CookcademyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CookcademyApp.swift
+//  Cookcademy
+//
+//  Created by Order on 7/22/22.
+//
+
+import SwiftUI
+
+@main
+struct CookcademyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
